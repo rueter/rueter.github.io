0ef5b1c58fc36827ea691282487d65c0a5ed321f
Jack Rueter <rueter.jack@gmail.com>
2022-08-15 20:01:22 +0300

    correction to mouseover

--- emerald/id4mouseover.js
+++ emerald/id4mouseover.js
@@ -1,4 +1,4 @@
-function jackMethod() {
+function id4mouseoverMethod() {
  var elements = document.getElementsByTagName("sentence");
 for (var i=0;i<elements.length;i++){
 elements[i].setAttribute("title",elements[i].id);
